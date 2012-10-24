060cccd0da569e7cb0457cd0a61cf9cb4b03ca23
xinsq <mail@xin-squared.com>
2012-10-24 18:32:26 -0400

    Add ability to see list of posts

--- dwa/p2.electricsignals.com/controllers/c_posts.php
+++ dwa/p2.electricsignals.com/controllers/c_posts.php
@@ -40,4 +40,27 @@ class posts_controller extends base_controller {
 		echo "Your post has been added. <a href='/posts/add'>Add another?</a>";
 	
 	}
+	
+	public function index() {
+ 
+	    # Set up view
+	    $this->template->content = View::instance('v_posts_index');
+	    $this->template->title   = "Posts";
+	    
+	    # Build our query
+	    $q = "SELECT * 
+	        FROM posts
+	        JOIN users USING (user_id)";
+	    
+	    # Run our query, grabbing all the posts and joining in the users    
+	    $posts = DB::instance(DB_NAME)->select_rows($q);
+	 
+	    # Pass data to the view
+	    $this->template->content->posts = $posts;
+	    
+	    # Render view
+	    echo $this->template;
+    
+    }
+
 }
