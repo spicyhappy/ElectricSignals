d91bc2a613b7325c93c0bcdbf9484d450ff2d3e4
xinsq <mail@xin-squared.com>
2012-10-29 16:55:27 -0400

    Index update

--- dwa/p2.electricsignals.com/views/v_index_index.php
+++ dwa/p2.electricsignals.com/views/v_index_index.php
@@ -1,6 +1,6 @@
-<pre>
-Hello World!
-Controller: c_index.php
-Method: index()
-View: v_index_index.php
-</pre>
+<h1>Pictochat</h1>
+<h2>Welcome, stranger. Pictochat is Twitter for photos. Post, respond, repost, and follow your favorite people!</h2>
+<ul>
+	<li><a href="/users/login">Log in</a></li>
+	<li><a href="/users/signup">Sign up</a></li>
+</ul>
