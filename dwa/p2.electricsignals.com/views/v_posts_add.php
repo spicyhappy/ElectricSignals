3f999cb6d42426c43e71a191094a6a7aa7154f64
xinsq <mail@xin-squared.com>
2012-10-31 17:57:33 -0400

    Change around locations

--- dwa/p2.electricsignals.com/views/v_posts_add.php
+++ dwa/p2.electricsignals.com/views/v_posts_add.php
@@ -3,12 +3,13 @@
 	<strong>New Post:</strong><br>
 	<textarea name='url'></textarea>
 
-	<br><br>
-	<input type='submit'>
 	<? if($error): ?>
 		<div class="error">Sorry that was not an image. Pictochat only takes jpeg, jpg, and png file formats.</div>
-		<br />
 	<? endif; ?>
+
+	<br><br>
+	<input type='submit'>
+
 	
 
 </form>
