c8bcf561fcd1a97d4528df8df8380d637d0e0c7f
xinsq <mail@xin-squared.com>
2012-10-31 18:29:55 -0400

    No Followers
    Give a message if you aren't following anyone.

--- dwa/p2.electricsignals.com/controllers/c_posts.php
+++ dwa/p2.electricsignals.com/controllers/c_posts.php
@@ -80,39 +80,48 @@ class posts_controller extends base_controller {
 		# Set up view
 		$this->template->content = View::instance('v_posts_index');
 		$this->template->title   = "Posts";
-		
+
 		# Build a query of the users this user is following - we're only interested in their posts
 		$q = "SELECT * 
 			FROM users_users
 			WHERE user_id = ".$this->user->user_id;
-		
+
 		# Execute our query, storing the results in a variable $connections
 		$connections = DB::instance(DB_NAME)->select_rows($q);
-		
+
 		# In order to query for the posts we need, we're going to need a string of user id's, separated by commas
 		# To create this, loop through our connections array
 		$connections_string = "";
 		foreach($connections as $connection) {
 			$connections_string .= $connection['user_id_followed'].",";
 		}
-		
+
 		# Remove the final comma 
 		$connections_string = substr($connections_string, 0, -1);
-		
+
 		# Connections string example: 10,7,8 (where the numbers are the user_ids of who this user is following)
-	
-		# Now, lets build our query to grab the posts
-		$q = "SELECT * 
+		if(empty($connections_string)) {
+			# If the user isn't following anyone, this prevents a SQL error
+			$no_followers = "You need to follow people to see their posts! Find some friends <a href=\"/posts/users\">here</a>.";
+			$this->template->content->no_followers = $no_followers;
+		} else {
+			# Run our query, store the results in the variable $posts
+			# If our user is following any users, we'll run the query to grab their posts
+			$q =
+			"SELECT images.*, users.user_id, users.first_name, users.last_name 
 			FROM images 
 			JOIN users USING (user_id)
-			WHERE images.user_id IN (".$connections_string.")"; # This is where we use that string of user_ids we created
-					
+			WHERE images.user_id IN (".$connections_string.") 
+			ORDER BY images.created DESC"; # This is where we use that string of user_ids we created
+			$posts = DB::instance(DB_NAME)->select_rows($q);
+		}
+
 		# Run our query, store the results in the variable $posts
 		$posts = DB::instance(DB_NAME)->select_rows($q);
-		
+
 		# Pass data to the view
 		$this->template->content->posts = $posts;
-		
+
 		# Render view
 		echo $this->template;
 		
--- dwa/p2.electricsignals.com/views/v_posts_index.php
+++ dwa/p2.electricsignals.com/views/v_posts_index.php
@@ -1,8 +1,18 @@
-<? foreach($posts as $post): ?>
-	
-	<h2><?=$post['first_name']?> <?=$post['last_name']?> posted:</h2>
-	<img src="<?=$post['url']?>" />
-	
-	<br><br>
-	
-<? endforeach; ?>
+
+
+
+<?if(isset($no_followers)):?>
+	<?echo $no_followers;?>
+<?else:?>
+
+	<?foreach($posts as $post):?>
+
+		<i><?=$post['first_name']?> <?=$post['last_name']?> posted on <?=$post['created']?>:</i>
+		<br>
+		<h3><img src="<?=$post['url']?>" /></h3>
+		<br>
+
+	<?endforeach;?>
+<?endif;?>
+
+
