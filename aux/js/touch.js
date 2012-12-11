30e9e51ce3a1477a75ed059f1b46171d11d848f7
xinsq <mail@xin-squared.com>
2012-12-10 22:59:21 -0500

    Add touch functionality

--- aux/js/touch.js
+++ aux/js/touch.js
@@ -1,3 +1,20 @@
 (function() {
 	
-})
+	var body = document.getElementsByTagName("body")[0];
+	
+	var touchControls = {
+		
+		pokeTheScreen : function() {
+			
+			body.innerHTML += "You just poked me, how rude!<br>";
+		},
+		
+		stopPokingTheScreen: function() {
+			body.innerHTMl += "Don't poke me again.<br><br>";
+		}
+	};
+	
+	body.addEventListener("touchstart", touchControls.pokeTheScreen, false);
+	body.addEventListener("touchend", touchControls.stopPokingTheScreen, false);
+	
+})();
